865dbd1fc61be551f29878737bb26b1ed9f4b5e3
MUKUL <mukul.2226csit1115@kiet.edu>
2025-04-12 08:43:35 +0530

    Updated Connections

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/calendar-app';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://aggarwalmukul2194:CenStageIntern@censtagecluster.gd6igtm.mongodb.net/';
 
 // Middleware
 app.use(express.json());
